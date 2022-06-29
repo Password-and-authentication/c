afdeb1a3b61310162a18c9be91b5c36559947192
kyopa <pomelleero@gmail.com>
2022-06-29 22:52:41 +0300

    strend w pointers

--- main.c
+++ main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
 
-void strcatt(char *s, char *t);
+int strend(char *s, char *t);
 
 int main() {
     
-    char s[100] = "hello";
-    char t[] = "lol";
-    strcatt(s, t);
-    printf("%s", s);
+    char s[100] = "skateboard";
+    char t[] = "board";
+    printf("%d", strend(s, t));
 
     return 0;
 }
 
-void strcatt(char *s, char *t) {
+int strend(char *s, char *t) {
+    int i = 0;
+    char *temp = t;
+    while (*++temp && ++i);
+    ++i;
     while (*++s);
-    while((*s++ = *t++));
+    s -= i;
+
+    while (*s) {
+        if (*s++ != *t++) return 0;
+    }
+    return 1;
+    
 }
 
