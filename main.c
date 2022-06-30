be28047f83184c13be6d2c94e54d0b1975eef700
kyopa <pomelleero@gmail.com>
2022-06-30 12:10:49 +0300

    strncopy

--- main.c
+++ main.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
 
-int strend(char *s, char *t);
+char* strncopy(char *s, char *t, int n);
 
 int main() {
     
-    char s[100] = "skateboard";
-    char t[] = "board";
-    printf("%d", strend(s, t));
+    char t[] = "ksi";
+    char s[100];
+    char *ptr = strncopy(s, t, 23);
+    printf("%s", ptr);
 
     return 0;
 }
 
-int strend(char *s, char *t) {
-    int i = 0;
-    char *temp = t;
-    while (*++temp && ++i);
-    ++i;
-    while (*++s);
-    s -= i;
-
-    while (*s) {
-        if (*s++ != *t++) return 0;
-    }
-    return 1;
-    
+char *strncopy(char *s, char *t, int n) {
+    char *temp = s;
+    while(n-- && (*s++ = *t++));
+    return temp;
 }
+
+
 
