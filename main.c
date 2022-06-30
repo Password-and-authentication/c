bc230145086e06e530861bfcce9b90470e49ad94
kyopa <pomelleero@gmail.com>
2022-06-30 12:29:36 +0300

    strncmp

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
 
-
-char *strngcat(char *s, char *t, int n);
+int strngcmp(char *s, char *t, int n);
 
 int main() {
     
-    char s[100] = "sidemen";
-    char t[] = "ksi";
-    char *ptr = strngcat(s, t, 3);
-    printf("%s", ptr);
+    char s[100] = "cccc";
+    char *t = "ccc";
+    printf("%d", strngcmp(s, t, 5));
+    
     
 
     return 0;
 }
 
-char *strngcat(char *s, char *t, int n) {
-    char *ret = s;
-    while(*s++);
-    --s;
-    while(n-- && (*s++ = *t++));
-    *s = 0;
-    return ret;
+int strngcmp(char *s, char *t, int n) {
+    while (n-- && (*s && *t)) {
+        if (*s++ < *t++) return -1;
+        if (*t < *s) return 1;
+    }
+    return 0;
 }
+
 
 
 
