d13fe0b9960a044f80ea066e3989062f899bae6b
kyopa <pomelleero@gmail.com>
2022-06-30 14:30:16 +0300

    readlines

--- main.c
+++ main.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
 
 
-int strngcmp(char *s, char *t, int n);
+void readlines(char *ptrarr[], char lines[]);
 
 int main() {
     
-    char s[100] = "cccc";
-    char *t = "ccc";
-    printf("%d", strngcmp(s, t, 5));
-    
+    char *ptrarr[200];
+    char lines[10000];
+    readlines(ptrarr, lines);
+    char *c = *ptrarr;
+    putchar(*c);                
     
 
     return 0;
 }
 
-int strngcmp(char *s, char *t, int n) {
-    while (n-- && (*s && *t)) {
-        if (*s++ < *t++) return -1;
-        if (*t < *s) return 1;
+void readlines(char *ptrarr[], char lines[]) {
+    char line[100];
+    char *c = line;
+    int len = 1;
+    while (++len && (*c++ = getchar()) != EOF) {
+
+        *lines++ = *(c - 1);
+        if (*(c - 1) == 10) {
+            len -= 2;          
+            *c = 0;
+            c = line;
+            *ptrarr++ = lines - len - 1;
+            len = 1;
+        }
     }
-    return 0;
 }
+
+
+
+
+
+
+
 
 
 
